de3ddaeb4deb737634b011efac034ef161e32a05
Sam Kronick <sam.kronick@gmail.com>
2011-05-16 21:28:03 +0000

    Initial commit.

---
+++ db-connect.php
@@ -0,0 +1,12 @@
+<?
+require_once("settings.php");
+require_once("json-response.php");
+
+$databaseConnection = mysql_connect($SETTINGS["DATABASE_HOST"],
+																		$SETTINGS["DATABASE_USER"],
+																		$SETTINGS["DATABASE_PASSWORD"]);
+
+if(!$databaseConnection || !mysql_select_db($SETTINGS["DATABASE_NAME"])) {
+	json_error(503, "Could not connect to database.");
+}
+?>
